cc58b7cde4c71019faa61eb36a86f74695515586
Rory Humphries <rory@humphries.ie>
2020-10-22 00:32:20 +0100

    Fixing submodules

---
+++ data_processing/create_gif.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 22 10:35:36 2020
+
+@author: roryh
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 27 09:02:56 2020
+
+@author: roryh
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import datetime
+
+import os
+import geopandas as gpd
+import imageio
+from shapely import wkt
+
+#######################################
+data_path = '../output/'
+gif_name = 'output.gif'
+
+cmap = 'viridis'
+max_time = 500
+cb_axis_title = 'No. of infected per county'
+
+agg_by_county = True
+per_area = False
+output_gif = True
+#######################################
+
+files = []
+for filename in os.listdir(data_path):
+    files += [filename]
+    
+start_date = datetime.datetime(2020, 1, 22)
+dates = [start_date + datetime.timedelta(days=x) for x in range(max_time)]
+
+# read in shapefile
+ed_soa_gdf = pd.read_csv('../data/processed/ed_soa_data_frame.csv')
+ed_soa_gdf['geometry'] = ed_soa_gdf['geometry'].apply(wkt.loads)
+ed_soa_gdf = gpd.GeoDataFrame(ed_soa_gdf, crs='epsg:4326')
+ed_soa_gdf = ed_soa_gdf.to_crs('epsg:29902')
+
+ed_soa_gdf['area'] = ed_soa_gdf.area/1000
+
+if agg_by_county:
+    ed_soa_gdf['area'] = ed_soa_gdf.groupby('county')['area'].transform(sum)
+
+
+# get max values
+maxs = 0;maxi = 0;maxr = 0;maxd = 0;maxx = 0;mini = 1
+for filename in range(1, max(max_time, len(files))):
+    df = pd.read_csv(data_path+str(filename)+".csv")
+    maxs = max(maxs, max(df.S))
+    maxi = max(maxi, max(df.I))
+    maxr = max(maxr, max(df.R))
+    maxd = max(maxd, max(df.D))
+    maxx = max(maxx, max(df.X))
+    
+
+images = []
+for filename in range(1, min(max_time, len(files))):
+    print(filename)
+    fig, ax = plt.subplots(1, 1, figsize = (10,10))
+    df = pd.read_csv(data_path+str(filename)+".csv")
+   
+    N = df.N
+    ed_soa_gdf['flag'] = 1
+    ed_soa_gdf['I'] = df.I.to_numpy()
+    
+    if agg_by_county:
+        ed_soa_gdf['I'] = ed_soa_gdf.groupby('county')['I'].transform(sum)
+        
+    if per_area:
+        ed_soa_gdf['I'] /= ed_soa_gdf['area']
+    
+    
+    
+    #lognorm = matplotlib.colors.LogNorm(1, maxi)
+    lognorm = matplotlib.colors.Normalize(0, 200)
+
+
+    #ax2=ed_soa_gdf.plot(ax=ax, linewidth=0.0001, column = 'I', cmap = plt.get_cmap('YlOrRd', len(bounds)), norm=lognorm, legend = False)
+    ax2=ed_soa_gdf.plot(ax=ax, linewidth=0.0001, column = 'I', cmap = plt.get_cmap(cmap), norm=lognorm, legend = False)
+    patches = ax2.collections[0]
+    cbar = plt.colorbar(patches, ax=ax2, extend='max')
+    cbar.ax.set_ylabel(cb_axis_title, fontsize = 15)
+    cbar.ax.tick_params(labelsize=15) 
+    
+    
+    plt.title(dates[filename].strftime("%Y-%m-%d"), fontsize=20)
+    plt.axis('off')
+    plt.axis('equal')
+    plt.tight_layout()
+    if output_gif:
+        plt.savefig('tmp.png')
+        images.append(imageio.imread("tmp.png"))
+    plt.show()
+    
+if output_gif:
+    imageio.mimsave('output.gif', images[0:499])
+
+
+
+
---
+++ data_processing/timeseries.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 27 09:02:56 2020
+
+@author: roryh
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.cbook as cbook
+
+import datetime
+
+
+import os
+import geopandas as gpd
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
+
+######################
+save_figs = True
+
+data_path = '../output/'
+
+output_name = '2weeks_off_4weeks_on'
+fig_name = output_name + '.png'
+data_name = output_name + '.csv'
+title = '2 weeks lockdown 4 weeks free'
+max_time = 500
+#####################
+
+
+files = []
+for filename in os.listdir(data_path):
+    files += [filename]
+    
+hist_data = pd.read_csv("../data/raw/owid-covid-data.csv")
+hist_data = hist_data[hist_data.location == 'Ireland']
+hist_data.date = pd.to_datetime(hist_data.date)
+
+
+
+s=[];i=[];r=[];d=[];x=[]
+
+maxs = 0;maxi = 0;maxr = 0;maxd = 0;maxx = 0
+for filename in range(0, max_time):
+    df = pd.read_csv("../output/"+str(filename)+".csv")
+    s.append(sum(df.S))
+    i.append(sum(df.I))
+    r.append(sum(df.R))
+    d.append(sum(df.D))
+    x.append(sum(df.X))
+    
+maxs = max(s)
+maxi = max(i)
+maxr = max(r)
+maxd = max(d)
+maxx = max(x)
+    
+years = mdates.YearLocator()   # every year
+months = mdates.MonthLocator()  # every month
+years_fmt = mdates.DateFormatter('%Y')
+months_fmt = mdates.DateFormatter('%m')
+
+start_date = datetime.datetime(2020, 1,22)#+ datetime.timedelta(days=25)
+
+dates = [start_date + datetime.timedelta(days=k) for k in range(max_time)]
+
+#ax = plt.subplots(1, 1, figsize = (16/3, 9/3))
+fig, ax = plt.subplots(1, 1)
+plt.plot(dates, s, label = 'S', c = 'tab:purple')
+plt.plot(dates, np.array(i), label = 'I', c = 'tab:blue')
+plt.plot(dates, r, label = 'R', c = 'tab:orange')
+plt.plot(dates, d, label = 'D', c = 'tab:red')
+plt.plot(dates, x, label = 'X', c = 'tab:green')
+#plt.plot(dates[1:], -np.array(s[1:]) + np.array(s[:-1]), label = 'Model New Cases')
+#plt.plot(hist_data.date,hist_data.new_cases.to_numpy(), label = 'Actual New Cases')
+#plt.plot(np.array(dates[1:]), np.array(d[1:]) - np.array(d[:-1]), label = 'Model New Deaths')
+#plt.plot(hist_data.date, hist_data.new_deaths, '--', c = 'tab:red', 
+ #        alpha = 0.5, label = 'Actual New Deaths')
+#plt.plot(hist_data.date, hist_data.new_cases)
+
+
+event_durations = [50, 15, 52, 21, 21, 21, 21, 21, 400]
+
+event_dates = [start_date]
+for k in event_durations:
+    event_dates.append(event_dates[-1] + datetime.timedelta(days=k))
+    
+shade_event_dates = event_dates[1:-1]
+num_shade_colors = len(shade_event_dates)
+
+colors = plt.cm.viridis(np.linspace(0,1,num_shade_colors))
+
+for k in range(len(shade_event_dates)-1):
+    d1 = shade_event_dates[k]
+    d2 = shade_event_dates[k+1]
+    plt.axvspan(d1, d2, color = colors[k], alpha=0.2)
+
+ax.xaxis.set_major_locator(years)
+ax.xaxis.set_major_formatter(years_fmt)
+ax.xaxis.set_minor_locator(months)
+ax.xaxis.set_minor_formatter(months_fmt)
+ax.tick_params(which='major', length=16)
+plt.setp(ax.xaxis.get_minorticklabels(), rotation=45)
+#plt.setp(ax.xaxis.get_majorticklabels(), rotation=45)
+
+plt.title(title)
+plt.xlabel('Time [months]')
+plt.ylabel('No. of individuals')
+
+plt.ylim(1, maxs + 1000000)
+plt.yscale('log')
+plt.legend()
+
+if save_figs == True:
+    plt.savefig(fig_name, bbox_inches='tight', dpi = 300)
+
+pd.DataFrame({'S':s,'I':i,'X':x,'R':r,'D':d}).to_csv(data_name)
